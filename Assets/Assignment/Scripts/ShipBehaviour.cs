f79bce7770502e1bce9c9b0653e502e912354ab8
BrookieGG <brooke4444@me.com>
2025-03-28 16:08:27 -0400

    Created a coroutine!

--- Assets/Assignment/Scripts/ShipBehaviour.cs
+++ Assets/Assignment/Scripts/ShipBehaviour.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ShipBehaviour : MonoBehaviour
 {
+    private Coroutine obstacleSpawn;
+    private IEnumerator spawnObject;
+    public float t;
+    public float timeToSpawn = 5;
+    public GameObject star;
+    public GameObject blackhole;
+    public GameObject asteroid;
+    private int currentSpawn;
+    private Transform spawnTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTransform = transform;
+        obstacleSpawn = StartCoroutine(SpawnTheObstacles());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnTheObstacles()
+    {
+        while (true)
+        {
+            spawnObject = ObstacleTimer();
+            yield return StartCoroutine(spawnObject);
+        }
+    }
+
+    private IEnumerator ObstacleTimer()
+    {
+        t = 0;
+        while (t < timeToSpawn)
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("obstacle spawns");
+        spawnTransform.position = new Vector2(Random.Range(-8, 8), Random.Range(-4, 4));
+        if (currentSpawn == 0)
+        {
+            Instantiate(star,spawnTransform);
+        }
+        else if (currentSpawn == 1)
+        {
+            Instantiate(asteroid, spawnTransform);
+        }
+        else 
+        {
+            Instantiate(blackhole, spawnTransform);
+        }
+        currentSpawn = (currentSpawn + 1)%3;
+
     }
 }
---
+++ Assets/Assignment/Scripts/Star.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public GameObject player;
+    public float radius = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool CalculateDistance()
+    {
+        float changeX = player.transform.position.x - transform.position.x;
+        float changeY = player.transform.position.y - transform.position.y;
+        //using 2D distance equation - https://en.wikipedia.org/wiki/Euclidean_distance
+        float distance = Mathf.Sqrt(Mathf.Pow(changeX, 2) + Mathf.Pow(changeY, 2));
+        return distance < radius;
+    }
+}
